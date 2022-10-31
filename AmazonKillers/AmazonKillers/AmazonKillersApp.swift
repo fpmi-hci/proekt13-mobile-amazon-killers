534aa379bbf8ee544076fc59241df868e83e6f5d
Adam <adambokun@gmail.com>
2022-10-31 16:26:58 +0300

    create new project

---
+++ AmazonKillers/AmazonKillers/AmazonKillersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmazonKillersApp.swift
+//  AmazonKillers
+//
+//  Created by Adam Bokun on 31.10.22.
+//
+
+import SwiftUI
+
+@main
+struct AmazonKillersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
